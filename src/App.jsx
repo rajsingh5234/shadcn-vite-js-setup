83e73f5ff2f963b4048d0af6b9a5817fea5a9d27
Raj Singh <raj02mar2001@gmail.com>
2024-11-17 21:04:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button';
+
+function App() {
+
+  return (
+    <>
+      <Button>Secondary</Button> <br /><br />
+      <Button variant="destructive">Here</Button> <br /><br />
+      <Button variant="secondary">Secondary</Button> <br /><br />
+      <Button variant="ghost">Secondary</Button> <br /><br />
+    </>
+  )
+}
+
+export default App
